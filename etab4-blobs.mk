a7aa1cd7ee546300fb2ef8efe960b4bcf0219e62
Can Emeç <cemec009@gmail.com>
2016-10-25 01:22:14 +0300

    Update and rename w30-blobs.mk to etab4-blobs.mk

--- etab4-blobs.mk
+++ etab4-blobs.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := device/ramos/w30
+LOCAL_PATH := device/generalmobile/etab4
     
 #Prebuilt Files
 PRODUCT_COPY_FILES += \
