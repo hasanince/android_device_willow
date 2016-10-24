6f464b70087f1a9e4d965c7ba4fcd3ae9c6e7085
Can Emeç <cemec009@gmail.com>
2016-10-25 01:18:48 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := w30
+PRODUCT_RELEASE_NAME := etab4
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
@@ -12,14 +12,14 @@ TARGET_SCREEN_WIDTH := 800
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/ramos/w30/full_w30.mk)
+$(call inherit-product, device/generalmobile/etab4/full_etab4.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := w30
-PRODUCT_NAME := cm_w30
-PRODUCT_BRAND := Ramos
-PRODUCT_MODEL := W30
-PRODUCT_MANUFACTURER := Ramos
+PRODUCT_DEVICE := etab4
+PRODUCT_NAME := cm_etab4
+PRODUCT_BRAND := GM
+PRODUCT_MODEL := ETAB4
+PRODUCT_MANUFACTURER := GM
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=w30 TARGET_DEVICE=w30 BUILD_FINGERPRINT="Ramos/W30/smdk4x12:4.1.2/W30/smdk4x12:user/test-keys" PRIVATE_BUILD_DESC="w30-user 4.1.2 W30 W30XXDLIB test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=etab4 TARGET_DEVICE=etab4 BUILD_FINGERPRINT="GM/ETAB4/smdk4x12:4.1.2/W30/smdk4x12:user/test-keys" PRIVATE_BUILD_DESC="w30-user 4.1.2 W30 W30XXDLIB test-keys"
