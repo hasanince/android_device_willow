d989c47e57bb1aac9856fc964aa3c7bbab5d5003
Can Emeç <cemec009@gmail.com>
2016-10-25 01:14:03 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ TARGET_BOOTLOADER_BOARD_NAME := smdk
 # Kernel
 # TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 # TARGET_KERNEL_CONFIG := cyanogenmod_i9300_defconfig
-TARGET_PREBUILT_KERNEL := device/ramos/w30/kernel
+TARGET_PREBUILT_KERNEL := device/generalmobile/etab4/kernel
 
 # Wifi
 WIFI_DRIVER := mt6620
@@ -42,4 +42,4 @@ BOARD_HAVE_SAMSUNG_WIFI := false
 BOARD_HAVE_SAMSUNG_BLUETOOTH := false
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := smdk4x12,W30,w30
+TARGET_OTA_ASSERT_DEVICE := smdk4x12,ETAB4,etab4
