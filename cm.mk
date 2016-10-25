e404712ac281844764fd6758e5177046ab391b90
Can Emeç <cemec009@gmail.com>
2016-10-25 10:08:01 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := etab4
+PRODUCT_RELEASE_NAME := willow
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
@@ -12,14 +12,15 @@ TARGET_SCREEN_WIDTH := 800
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/generalmobile/etab4/full_etab4.mk)
+$(call inherit-product, device/generalmobile/willow/full_willow.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := etab4
-PRODUCT_NAME := cm_etab4
+PRODUCT_DEVICE := willow
+PRODUCT_NAME := cm_willow
 PRODUCT_BRAND := GM
-PRODUCT_MODEL := ETAB4
+PRODUCT_MODEL := Willow
 PRODUCT_MANUFACTURER := GM
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=etab4 TARGET_DEVICE=etab4 BUILD_FINGERPRINT="GM/ETAB4/smdk4x12:4.1.2/W30/smdk4x12:user/test-keys" PRIVATE_BUILD_DESC="w30-user 4.1.2 W30 W30XXDLIB test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=willow TARGET_DEVICE=willow
+BUILD_FINGERPRINT="GM/Willlow/smdk4x12:4.1.2/Williow/smdk4x12:user/test-keys" PRIVATE_BUILD_DESC="willow-user 4.1.2 willow WillowXXDLIB test-keys"
